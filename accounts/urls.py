e933b53368e5f74a5d93e95af1c5d32d67927203
YI-LUO-YI <3423197317@qq.com>
2024-12-24 15:43:05 +0800

    19.2.4　登录页面

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+"""为应用程序 accounts 定义 URL 模式"""
+from django.urls import path, include
+app_name = 'accounts'
+
+urlpatterns = [
+    # 包含默认的身份验证 URL
+    path('', include('django.contrib.auth.urls')),
+]
--- ll_project/settings.py
+++ ll_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # 我的应用程序
     'learning_logs',
+    'accounts',
     
     # Django自带的应用程序
     'django.contrib.admin',
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 我的设置
+LOGIN_REDIRECT_URL = 'learning_logs:index'
