b4662e3accb6dd83226fb0692784e56da92c16a3
YI-LUO-YI <3423197317@qq.com>
2024-12-24 12:17:43 +0800

    练习 18.2

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -21,4 +21,6 @@ class Entry(models.Model):
         
     def __str__(self):
         """返回一个表示条目的简单字符串"""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        return self.text
