b0c2119b564ef883dd1d06e1c2109b064fc80b00
YI-LUO-YI <3423197317@qq.com>
2024-12-24 13:12:07 +0800

    18.4.3　显示特定主题的页面

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # 主页
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),   
 ]
