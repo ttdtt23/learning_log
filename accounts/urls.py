336837d8f17cea3781d3ec2eede19cd4955cf5b1
YI-LUO-YI <3423197317@qq.com>
2024-12-24 15:53:48 +0800

    19.2.6　注册页面

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ app_name = 'accounts'
 urlpatterns = [
     # 包含默认的身份验证 URL
     path('', include('django.contrib.auth.urls')),
+    # 注册页面
+    path('register/', views.register, name='register'),
 ]
