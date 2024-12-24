f97906fc4c006f7359ee19cba781022252ab9737
YI-LUO-YI <3423197317@qq.com>
2024-12-24 16:36:19 +0800

    19.3.6　将新主题关联到当前用户

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -37,7 +37,9 @@ def new_topic(request):
         # POST 提交的数据：对数据进行处理
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # 显示空表单或指出表单数据无效
