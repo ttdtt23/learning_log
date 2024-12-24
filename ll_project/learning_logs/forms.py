3326401021697b5e5612894efce913b0b80f1cfa
username <username@example.com>
2024-12-24 19:03:52 +0800

    20章完结

---
+++ ll_project/learning_logs/forms.py
@@ -0,0 +1,16 @@
+
+from django import forms
+from .models import Topic
+from .models import Topic, Entry
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ['text', 'public']
+        labels = {'text': '主题', 'public': '公开主题'}
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}        
